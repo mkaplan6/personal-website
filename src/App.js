859ba221de034072da458b5b67bbfc20482e588b
Tyler Cushing <tylercushing@wirelessprv-10-192-231-131.near.illinois.edu>
2022-10-16 22:14:33 -0500

    Changed the website text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Oooh I needed that
         </p>
         <a
           className="App-link"
