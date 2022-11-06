d356fa7dc4d3abc6ff6eb40814d7dd7ff1047cab
malco <mkaplan6@illinois.edu>
2022-11-06 17:14:15 -0600

    added image

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import { Routes, Route } from 'react-router-dom'
 import { Home } from './Components/Home';
 import { Resume } from './Components/Resume'
 import { Misc } from './Components/Misc'
+import banner from './images/banner.png';
 
 function App() {
   return (
-    <Routes> {/* Every route of the website goes in between these tags */}
+    <div>
+      <img src={banner} alt=""/>
+      <Routes> {/* Every route of the website goes in between these tags */}
       <Route path='/' element={<Home />}></Route> {/* Render one specific route of the website*/}
       <Route path='resume' element={<Resume />}></Route>
       <Route path='misc' element={<Misc />}></Route>
     </Routes>
+    </div>
   );
 }
 
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,6 +2,6 @@ import React from 'react'
 
 export const Home = () => {
   return (
-    <div style={{color: 'white'}}>Home</div>
+    <div style={{color: 'white'}}><b>Home</b></div>
   )
 }
