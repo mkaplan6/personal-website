abf09cd705d9aa767e3b5b8f507e23eff6a81338
Tyler Cushing <tylercushing@wirelessprv-10-192-97-167.near.illinois.edu>
2022-11-06 17:12:56 -0600

    Added in the files

---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+export default function Navbar() {
+    return <nav className="nav">
+        <a href="/" className="site-title">Malcolm</a>
+        <ul>
+            <li>
+                <a href="resume">Resume</a>
+            </li>
+            <li>
+                <a href="misc">Misc</a>
+            </li>
+        </ul>
+    </nav>
+}
