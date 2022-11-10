63881860e1b9b68c3ac913c5920c89be7e902123
malco <mkaplan6@illinois.edu>
2022-11-09 23:43:52 -0600

    Cleaned up

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -5,7 +5,7 @@ import banner from '.././images/banner.png';
     return (
         <div className='Res'>
             <object data = 'https://drive.google.com/file/d/1KP24nYSE6rFhWTwRwW52c__jchzXZ3CR/view?usp=sharing' type="application/pdf" width="100%" height={100}>
-            <iframe src="https://drive.google.com/file/d/1idKXVgShCrWk9ekyLLcV8OkMQAoMxybd/preview" width="100%" height="1100" allow="autoplay"></iframe>
+            <iframe src="https://drive.google.com/file/d/1SIzrj4OooWliTK4bCHbkzYaTIdebY_7r/preview" width="100%" height="1100" allow="autoplay"></iframe>
             </object>
             <img style={{position:"relative", bottom: -10, width:"100%", height:"100%"}}src={banner} alt=""/>
         </div>
