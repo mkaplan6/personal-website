a83d5aad9b1f55ecf003f74102652e787a073900
malco <mkaplan6@illinois.edu>
2022-12-01 17:08:58 -0600

    fixed text

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,7 +1,7 @@
 //import pfp from '.././images/pfp.jpg';
 import banner from '.././images/banner.png';
 //import text from '.././images/text.png';
-import home2 from '.././images/home2.png'
+import home3 from '.././images/home3.png'
 // import guitar from '.././images/guitar.jpg';
 // import drum from '.././images/drum.png';
 // import Counter from '../Counter';
@@ -12,7 +12,7 @@ export const Home = () => {
       {/*<img style={{position:"absolute", left: 350, bottom: 250, width:255.75, height:337}}src={guitar} alt=""/>
       <img style={{position:"absolute", left: 350, bottom: 25, width:255.75, height:337}}src={drum} alt=""/>*/}
       <img style={{position:"absolute", bottom: -300, width:1545, height:"60%"}}src={banner} alt=""/>
-      <img style={{position:"relative", left: 200, bottom: -50, width:1200, height:400}}src={home2} alt=""/>
+      <img style={{position:"relative", left: 200, bottom: -50, width:1200, height:400}}src={home3} alt=""/>
       {/* <img style={{position:"relative", left: 200, bottom: -50, width:306.9, height:404.4}}src={pfp} alt=""/>
       <img style={{position:"relative", left: 300, bottom: -50, width:875, height:400}}src={text} alt=""/> */}
       {/* <h2 style={{position:"relative", left: 735, bottom:350, textAlign: 'left', color:"white"}}><b>About Me</b></h2>
